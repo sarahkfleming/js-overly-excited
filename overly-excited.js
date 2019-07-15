3830dc523eb40dbd8dd2c4cb6dc9450909b9192b
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-15 14:06:08 -0500

    Part 3 of Exercise

--- overly-excited.js
+++ overly-excited.js
@@ -1,21 +1,18 @@
-// Practice: Some words are more exciting than others
+// Practice: Function Arguments
+// The learning objective of this exercise is to practice defining and using function arguments.
 
-// The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one operation when a certain conditions is true, and a different operation if that condition is false.
+// Add a new argument to the function so that a developer can specify which character should be displayed instead of it always being an exclamation point.
 
 // Create an array that contains the words in the sentence
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-// Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
-
-// If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
-
-function addExcitement(theWordArray) {
+function addExcitement(theWordArray, punctuation, times) {
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
         // If the current word's place (not index) in the array is evenly divisible by 3, add an exclamation point to the end of the word and then concatenate it to `buildMeUp`.
         if ((i + 1) % 3 === 0) {
-            buildMeUp += ` ${theWordArray[i]} !`
+            buildMeUp += ` ${theWordArray[i]}${punctuation.repeat(times)}`
         }
             // Otherwise, just concatenate the word itself.
             else {
@@ -25,4 +22,4 @@ function addExcitement(theWordArray) {
         console.log(buildMeUp)
     }
 }
-addExcitement(sentence)
+addExcitement(sentence, "*", 4)
