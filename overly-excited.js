674dc417602ebc657f976361aa3f9e8965047bf0
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-15 14:04:59 -0500

    Part 2 of Exercise

--- overly-excited.js
+++ overly-excited.js
@@ -1,26 +1,28 @@
-// Practice: Stacking Words
-// The learning objects of this exercise is to use iteration and string concatenation
+// Practice: Some words are more exciting than others
+
+// The learning objective of this exercise is to use the JavaScript if statement inside a loop to do one operation when a certain conditions is true, and a different operation if that condition is false.
 
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray) {
+// Add logic to addExcitement that places an exclamation point (!) after every third word. Read the following English statement and write the equivalent in JavaSript code to make it work.
 
-    // Each time the for loop executes, you're going to add one more word to this string
+// If the current value of the counter variable can be evenly divided by 3 (using the JavaScript remainder operator) add a single exclamation point (!) to the current word in the array.
+
+function addExcitement(theWordArray) {
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        const walrusString = buildMeUp += ` ${theWordArray[i]}`
-
+        // If the current word's place (not index) in the array is evenly divisible by 3, add an exclamation point to the end of the word and then concatenate it to `buildMeUp`.
+        if ((i + 1) % 3 === 0) {
+            buildMeUp += ` ${theWordArray[i]} !`
+        }
+            // Otherwise, just concatenate the word itself.
+            else {
+            buildMeUp += ` ${theWordArray[i]}`
+        }
         // Print buildMeUp to the console
-        console.log(walrusString)
+        console.log(buildMeUp)
     }
 }
-// Invoke the function and pass in the array
 addExcitement(sentence)
